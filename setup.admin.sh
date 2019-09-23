7f8991e42ef70d70783f82deb1edde22106f2eaf
EddieB <eddiebergmanhs@gmail.com>
2019-09-23 17:11:58 +0000

    Replaced tabs with spaces, removed trailing spaces

--- setup.admin.sh
+++ setup.admin.sh
@@ -1,6 +1,6 @@
 # Includes:
-# 	core: ufw, zsh, git, nvim
-# 
+#     core: ufw, zsh, git, nvim
+#
 # Should be enough for going onto most new machines and
 # doing basic work
 sudo echo "Sudo mode" >/dev/null
@@ -15,14 +15,14 @@ CUR_DIR=$(dirname "$(readlink -f "$0")")
 source $CUR_DIR/.zsh/.zshenv
 
 if [[ -z "$DOT_DIR" ]]; then
-	echo ".zshenv did not load properly or is missing \$DOT_DIR, please investigate"
-	exit 1
+    echo ".zshenv did not load properly or is missing \$DOT_DIR, please investigate"
+    exit 1
 fi
 
 if [[ "$CUR_DIR" != "$DOT_DIR" ]]; then
-	echo ".dot doesn't seems to be mouted at $DOT_DIR, mounted at $CUR_DIR"
-	echo "- If $DOT_DIR doesnt seem correct, try running without sudo '$ ./setup.core.sh'"
-	exit 1
+    echo ".dot doesn't seems to be mouted at $DOT_DIR, mounted at $CUR_DIR"
+    echo "- If $DOT_DIR doesnt seem correct, try running without sudo '$ ./setup.core.sh'"
+    exit 1
 fi
 
 SETUP_SCRIPT=true
@@ -30,7 +30,7 @@ SETUP_TYPE="admin"
 
 SETUP_LOG="$DOT_DIR/log/setuplog.txt"
 if [[ ! -d "$DOT_DIR/log" ]]; then
-	mkdir -p $DOT_DIR/log
+    mkdir -p $DOT_DIR/log
 fi
 
 #echo "Installation began at: $(date -u)" >> "$SETUP_LOG"
@@ -46,8 +46,8 @@ sudo chmod -R u+x $DOT_DIR/installers
 ####################
 # Installation
 ####################
-echo "Installation start, may take a while. Please see $SETUP_LOG \ 
-	for progress updates or potential errors"
+echo "Installation start, may take a while. Please see $SETUP_LOG \
+    for progress updates or potential errors"
 
 ./installers/ufw.install.sh
 ./installers/zsh.install.sh
