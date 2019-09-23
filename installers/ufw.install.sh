6972d2f22c92e3781dc64b739596ddb519a768a3
EddieB <eddiebergmanhs@gmail.com>
2019-09-23 14:57:02 +0000

    ufw installer now activates even if already installed, reduced logging

--- installers/ufw.install.sh
+++ installers/ufw.install.sh
@@ -5,9 +5,7 @@ fi
 
 if ! command -v ufw >/dev/null; then
 	sudo pacman -Syu ufw --noconfirm --needed >> "$SETUP_LOG"
-	sudo ufw enable
-	sudo systemctl enable ufw.service
-	echo "ufw: Installed"
-else
-	echo "ufw: Already Installed"
 fi
+
+sudo ufw enable >> "$SETUP_LOG"
+sudo systemctl enable ufw.service >> "$SETUP_LOG"
