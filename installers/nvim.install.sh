ee4cef53cda3cd206c1ed56dbfd74c199eb3061a
EddieB <eddiebergmanhs@gmail.com>
2019-09-23 16:39:43 +0000

    Updated nvim installer to install core or dekstop version

--- installers/nvim.install.sh
+++ installers/nvim.install.sh
@@ -1,11 +1,34 @@
 if [[ ! "$SETUP_SCRIPT" ]]; then
-	echo "Please run from one of the setup scripts"
-	exit 1
+    echo "Please run from one of the setup scripts"
+    exit 1
 fi
 
 if ! command -v nvim >/dev/null; then
-	pacman -Syu neovim --needed --noconfirm >> "$SETUP_LOG"
-fi	
+    pacman -Syu neovim --needed --noconfirm >> "$SETUP_LOG"
+fi
 
-ln -sfn $DOT_DIR/.vim/.vimrc $HOME/.vimrc
-ln -sfn $DOT_DIR/.vim $HOME/.vim
+# Nvim setup to point to placed vimrc
+if [[ ! -d "$HOME/.config/nvim" ]]; then
+    mkdir -p $HOME/.config/nvim
+fi
+
+ln -sfn $DOT_DIR/.vim/.nvim_init.vim $HOME/.config/nvim/init.vim
+
+if [[ "$SETUP_TYPE" == "core" ]]; then
+    ln -sfn $DOT_DIR/.vim/.vimrc_core $HOME/.vimrc
+
+elif [[ "$SETUP_TYPE" == "desktop" ]]; then
+    ln -sfn $DOT_DIR/.vim/.vimrc $HOME/.vimrc
+
+    [[ -d "$HOME/.vim" ]] || mkdir -p $HOME/.vim
+    ln -sfn $DOT_DIR/.vim/snips $HOME/.vim/snips
+
+    VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
+    [[ -d "$VUNDLE_DIR" ]] || rm -rf "$VUNDLE_DIR"
+    git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR" >> "$SETUP_LOG"
+    nvim +PluginInstall +qall >> "$SETUP_LOG"
+else
+    echo "Unrecognized SETUP_TYPE $SETUP_TYPE, exiting installation"
+    exit 1
+fi
+
