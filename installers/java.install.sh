e1bf200193ed41f3ceaec04b79ad8bb5767fa5c7
EddieB <eddiebergmanhs@gmail.com>
2019-09-27 18:05:55 +0000

    Added java installer

---
+++ installers/java.install.sh
@@ -0,0 +1,8 @@
+if [[ ! "$SETUP_SCRIPT" ]]; then
+    echo "Please run from one of the setup scripts"
+    exit 1
+fi
+
+if ! pacman -Qi jdk12-openjdk > /dev/null 2>&1 then;
+    sudo pacman -Syu jdk12-openjdk --noconfirm --needed >> "$SETUP_LOG"
+fi
