a113e2c993b4b02484e35d15ea0ce898a0dfbcce
EddieB <eddiebergmanhs@gmail.com>
2019-09-29 13:09:04 +0000

    Added python to development installer

--- installers/development.install.sh
+++ installers/development.install.sh
@@ -10,3 +10,12 @@ fi
 if ! pacman -Qi jdk12-openjdk > /dev/null 2>&1 then;
     sudo pacman -Syu jdk12-openjdk --noconfirm --needed >> "$SETUP_LOG"
 fi
+
+# Python stuff
+if ! command -v python > /dev/null 2>&1; then
+    sudo pacman -Syu python --noconfim --needed >> "$SETUP_LOG"
+fi
+
+if ! command -v pip > /dev/null 2>&1; then
+    sudo pacman -Syu python-pip --noconfirm --needed >> "$SETUP_LOG"
+fi
