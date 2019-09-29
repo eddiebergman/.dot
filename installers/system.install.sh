279a65233b956cf52ca869aa566a4ae88511125e
EddieB <eddiebergmanhs@gmail.com>
2019-09-29 23:19:08 +0000

    Added rofi for application launching and its config

--- installers/system.install.sh
+++ installers/system.install.sh
@@ -7,10 +7,6 @@ if ! command -v light > /dev/null 2>&1; then
     sudo pacman -Syu light --noconfirm --needed >> "$SETUP_LOG"
 fi
 
-if ! command -v NetworkManager > /dev/null 2>&1; then
-    sudo pacman -Syu networkmanager --noconfirm --needed >> "$SETUP_LOG"
-fi
-
 if ! command -v nm-applet > /dev/null 2>&1; then
     sudo pacman -Syu network-manager-applet --noconfirm --needed >> "$SETUP_LOG"
 fi
@@ -18,6 +14,9 @@ fi
 if ! command -v rofi > /dev/null 2>&1; then
     sudo pacman -Syu rofi --noconfirm --needed >> "$SETUP_LOG"
 fi
+
+[[ -d "$HOME/.config/rofi" ]] || mkdir -p $HOME/.config/rofi
+ln -sfn $DOT_DIR/configs/rofi_config $HOME/.config/rofi/config
 
 # Include mime type for markdown
 ln -sfn $DOT_DIR/mimetypes/text-markdown.xml $HOME/.local/share/mime/packages/text-markdown.xml
