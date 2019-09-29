89183026401dc54ca4960a9d7d1e7a48ca7af84f
EddieB <eddiebergmanhs@gmail.com>
2019-09-29 22:35:46 +0000

    Added rofi, networkmanager, nm-applet

--- installers/system.install.sh
+++ installers/system.install.sh
@@ -7,6 +7,18 @@ if ! command -v light > /dev/null 2>&1; then
     sudo pacman -Syu light --noconfirm --needed >> "$SETUP_LOG"
 fi
 
+if ! command -v NetworkManager > /dev/null 2>&1; then
+    sudo pacman -Syu networkmanager --noconfirm --needed >> "$SETUP_LOG"
+fi
+
+if ! command -v nm-applet > /dev/null 2>&1; then
+    sudo pacman -Syu network-manager-applet --noconfirm --needed >> "$SETUP_LOG"
+fi
+
+if ! command -v rofi > /dev/null 2>&1; then
+    sudo pacman -Syu rofi --noconfirm --needed >> "$SETUP_LOG"
+fi
+
 # Include mime type for markdown
 ln -sfn $DOT_DIR/mimetypes/text-markdown.xml $HOME/.local/share/mime/packages/text-markdown.xml
 update-mime-database ~/.local/share/mime
