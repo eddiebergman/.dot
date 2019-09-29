e4ace7f367a1331eeff9a3b42f654d2c19e63b25
EddieB <eddiebergmanhs@gmail.com>
2019-09-29 13:09:24 +0000

    Added feh for image viewing

--- installers/apps.install.sh
+++ installers/apps.install.sh
@@ -5,4 +5,5 @@ fi
 
 ( command -v firefox > /dev/null 2>&1 ) || ( sudo pacman -Syu firefox --noconfirm --needed >> "$SETUP_LOG" )
 ( command -v zathura > /dev/null 2>&1 ) || ( sudo pacman -Syu zathura --noconfirm --needed >> "$SETUP_LOG" )
-( pacman -Qi libreoffice-still > /dev/null 2>&1 ) || ( sudo pacman -Syu libreoffice-still --noconfirm --needed "$SEUP_LOG" )
+( pacman -Qi libreoffice-still > /dev/null 2>&1 ) || ( sudo pacman -Syu libreoffice-still --noconfirm --needed "$SETUP_LOG" )
+( command -v feh > /dev/null 2>&1 ) || ( sudo pacman -Syu feh --noconfirm --needed "$SETUP_LOG" )
