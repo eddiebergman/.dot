b48cad89edf89f176b2e801f7600988a73c908b1
EddieB <eddiebergmanhs@gmail.com>
2019-09-29 13:48:16 +0000

    Added recognition for markdown

--- installers/system.install.sh
+++ installers/system.install.sh
@@ -6,3 +6,7 @@ fi
 if ! command -v light > /dev/null 2>&1; then
     sudo pacman -Syu light --noconfirm --needed >> "$SETUP_LOG"
 fi
+
+# Include mime type for markdown
+ln -sfn $DOT_DIR/mimetypes/text-markdown.xml $HOME/.local/share/mime/packages/text-markdown.xml
+update-mime-database ~/.local/share/mime
