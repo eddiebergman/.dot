3a132df8e4c5cd31ae3824f163dd42e246e4218d
EddieB <eddiebergmanhs@gmail.com>
2019-09-30 00:24:29 +0000

    Moved display to system installer

--- installers/system.install.sh
+++ installers/system.install.sh
@@ -3,6 +3,19 @@ if [[ ! "$SETUP_SCRIPT" ]]; then
     exit 1
 fi
 
+if ! pacman -Qg xorg > /dev/null 2>&1; then
+    sudo pacman -Syu xorg --noconfirm --needed >> "$SETUP_LOG"
+fi
+
+ln -sfn $CONFIG_DIR/.xinitrc $HOME/.xinitrc
+
+if ! pacman -Qg i3 > /dev/null 2>&1; then
+    sudo pacman -Syu i3 --noconfirm --needed >> "$SETUP_LOG"
+fi
+
+[[ -d "$HOME/.config/i3" ]] || mkdir -p $HOME/.config/i3
+ln -sfn $CONFIG_DIR/.i3_config $HOME/.config/i3/config
+
 if ! command -v light > /dev/null 2>&1; then
     sudo pacman -Syu light --noconfirm --needed >> "$SETUP_LOG"
 fi
