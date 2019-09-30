9c33b3c57983254e1b50eb1fc613a1ea75150ec0
EddieB <eddiebergmanhs@gmail.com>
2019-09-30 23:16:04 +0000

    Added latexmkrc, made zathura default pdfviewer

--- installers/tex.install.sh
+++ installers/tex.install.sh
@@ -5,3 +5,5 @@ fi
 
 ( pacman -Qg texlive-most > /dev/null 2>&1 ) || ( sudo pacman -Su texlive-most --noconfirm --needed >> "$SETUP_LOG" )
 ( pacman -Qi biber > /dev/null 2>&1 ) || ( sudo pacman -Su biber --noconfirm --needed >> "$SETUP_LOG")
+
+ln -sfn $DOT_DIR/.latexmkrc $HOME/.latexmkrc
