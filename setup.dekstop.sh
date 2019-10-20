aeab8a7a073de454cae28b9f9f5303df2f4978df
EddieB <eddiebergmanhs@gmail.com>
2019-10-20 01:18:14 +0000

    Cleaned up setup script

--- setup.dekstop.sh
+++ setup.dekstop.sh
@@ -39,19 +39,14 @@ sudo systemctl enable ufw.service >> "$SETUP_LOG"
 ( command -v ssh-keygen > /dev/null 2>&1 || sudo pacman -Su openssh --noconfirm --needed >> "$SETUP_LOG" )
 ( command -v xclip > /dev/null 2>&1 || sudo pacman -Su xclip --noconfirm --needed >> "$SETUP_LOG" )
 ( command -v light > /dev/null 2>&1 || sudo pacman -Su light --noconfirm --needed >> "$SETUP_LOG" )
-sudo usermod -a -G video "$USER" # Enables lighting control
+( command -v most > /dev/null 2>&1 || sudo pacman -Su most --noconfirm --needed >> "$SETUP_LOG" )
 
 #
 # display - on the tenth day, he gave un to us windows (not the OS, that was the 15th day)
 #
 ( pacman -Qg xorg > /dev/null 2>&1 || sudo pacman -Su xorg --noconfirm --needed >> "$SETUP_LOG" )
-ln -sfn $DOT_DIR/.xinitrc $HOME/.xinitrc
 
 ( pacman -Qg i3 > /dev/null 2>&1 || sudo pacman -Su i3 --noconfirm --needed >> "$SETUP_LOG" )
-[[ -d "$HOME/.config/i3" ]] || mkdir -p $HOME/.config/i3
-[[ -d "$HOME/.config/i3status" ]] || mkdir -p $HOME/.config/i3status
-ln -sfn $CONFIG_DIR/i3/config $HOME/.config/i3/config
-ln -sfn $CONFIG_DIR/i3status/config $HOME/.config/i3status/config
 
 ( command -v compton > /dev/null 2>&1 || sudo pacman -Su compton --noconfirm --needed >> "$SETUP_LOG" )
 
@@ -59,47 +54,37 @@ ln -sfn $CONFIG_DIR/i3status/config $HOME/.config/i3status/config
 # Apps - appra-kadabra
 #
 ( command -v rofi > /dev/null 2>&1 && sudo pacman -Su rofi --noconfirm --needed >> "$SETUP_LOG" )
-[[ -d "$HOME/.config/rofi" ]] || mkdir -p $HOME/.config/rofi
-ln -sfn $CONFIG_DIR/rofi/config $HOME/.config/rofi/config
 
 ( command -v firefox > /dev/null 2>&1 || sudo pacman -Su firefox --noconfirm --needed >> "$SETUP_LOG" )
 
-# Zathura file viewing
-( command -v zathura > /dev/null 2>&1 || sudo pacman -Su zathura --noconfirm --needed >> "$SETUP_LOG" )
-[[ -f "/usr/lib/zathura/libpdf-mupdf.so" ]] || ( sudo pacman -Su zathura-pdf-mupdf --noconfirm --needed >> "$SETUP_LOG")
-
 ( pacman -Qi libreoffice-still > /dev/null 2>&1 || sudo pacman -Su libreoffice-still --noconfirm --needed "$SETUP_LOG" )
-( command -v feh > /dev/null 2>&1 || sudo pacman -Su feh --noconfirm --needed "$SETUP_LOG" )
 ( command -v xfce4-screenshooter > /dev/null 2>&1 || sudo pacman -Su xfce4-screenshooter --noconfirm --needed "$SETUP_LOG" )
 
 #
-# zsh - shell
+# File Viewers - The alternative is to view them in byte sized chunks
+#
+( command -v zathura > /dev/null 2>&1 || sudo pacman -Su zathura --noconfirm --needed >> "$SETUP_LOG" )
+[[ -f "/usr/lib/zathura/libpdf-mupdf.so" ]] || ( sudo pacman -Su zathura-pdf-mupdf --noconfirm --needed >> "$SETUP_LOG")
+
+( command -v feh > /dev/null 2>&1 || sudo pacman -Su feh --noconfirm --needed "$SETUP_LOG" )
+( command -v evince > /dev/null 2>&1 || sudo pacman -Su evince --noconfirm --needed "$SETUP_LOG" )
+
+#
+# zsh - she sells zshells by the zshore
 #
 ( command -v zsh > /dev/null 2>&1 || sudo pacman -Su zsh zsh-completions --noconfirm --needed >> "$SETUP_LOG" )
-ln -sfn $ZSH_DIR/.zshenv $HOME/.zshenv
-ln -sfn $ZSH_DIR/.zshrc $HOME/.zshrc
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k $SHARE_DIR/powerlevel10k
 
 #
 # git - you should know what this is
 #
 ( command -v git > /dev/null 2>&1  || sudo pacman -Su git --noconfirm --needed >> "$SETUP_LOG" )
-ln -sfn $DOT_DIR/.gitconfig $HOME/.gitconfig
 
 #
 # nvim - edit text you say?
 #
 ( command -v nvim > /dev/null 2>&1  || sudo pacman -Su neovim python-pynvim --needed --noconfirm >> "$SETUP_LOG" )
-( [[ -d "$HOME/.config/nvim" ]] || mkdir -p $HOME/.config/nvim )
-( [[ -d "$HOME/.vim" ]] || mkdir -p $HOME/.vim )
 
-ln -sfn $VIM_DIR/.vimrc $HOME/.vimrc
-ln -sfn $VIM_DIR/snips $HOME/.vim/snips
-ln -sfn $VIM_DIR/ftplugin $HOME/.vim/ftplugin
-ln -sfn $VIM_DIR/plugin $HOME/.vim/plugin
-ln -sfn $VIM_DIR/template $HOME/.vim/template
-ln -sfn $CONFIG_DIR/nvim/init.vim $HOME/.config/nvim/init.vim
 
 VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
 ( [[ -d "$VUNDLE_DIR" ]] || rm -rf "$VUNDLE_DIR" )
@@ -121,7 +106,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR" >> "$SETUP_L
 #
 ( command -v python > /dev/null 2>&1) || (sudo pacman -Su python --noconfim --needed >> "$SETUP_LOG" )
 ( command -v pip > /dev/null 2>&1 || sudo pacman -Su python-pip --noconfirm --needed >> "$SETUP_LOG" )
-([[ -d "$HOME/venvs/py" ]] || ( mkdir -p "$HOME/venvs"  && python -m venv "$HOME/venvs/py" )
+([[ -d "$HOME/venvs/py" ]]) || ( mkdir -p "$HOME/venvs"  && python -m venv "$HOME/venvs/py" )
 
 #
 # Javascript - lets hope I never have to use any of this
@@ -134,25 +119,40 @@ git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR" >> "$SETUP_L
 #
 ( pacman -Qg texlive-most > /dev/null 2>&1 || sudo pacman -Su texlive-most --noconfirm --needed >> "$SETUP_LOG" )
 ( pacman -Qi biber > /dev/null 2>&1 || sudo pacman -Su biber --noconfirm --needed >> "$SETUP_LOG")
-ln -sfn $DOT_DIR/.latexmkrc $HOME/.latexmkrc
 
 #
-# Kitty - dog, cat or terminal kind of person?
+# Kitty - are you a dog, cat or terminal kind of person?
 # 
 ( command -v kitty > /dev/null 2>&1 || sudo pacman -Su kitty --noconfirm --needed >> "$SETUP_LOG")
-( [[ -d $HOME/.config/kitty ]] || mkdir -p $HOME/.config/kitty )
-ln -sfn $CONFIG_DIR/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 
+#
+# Linking - All the right things in all the right places
+#
+# Dirs
+ln -sfn $CONFIG_DIR/i3 $HOME/.config
+ln -sfn $CONFIG_DIR/nvim $HOME/.config
+ln -sfn $CONFIG_DIR/rofi $HOME/.config
+ln -sfn $CONFIG_DIR/kitty $HOME/.config
+ln -sfn $CONFIG_DIR/i3status $HOME/.config
+
+ln -sfn $VIM_DIR $HOME
+
+# Files
+ln -sfn $ZSH_DIR/.zshrc $HOME/.zshrc
+ln -sfn $ZSH_DIR/dircolors.256dark $HOME/.dir_colors
+
+ln -sfn $DOT_DIR/.xinitrc $HOME/.xinitrc
+ln -sfn $DOT_DIR/.latexmkrc $HOME/.latexmkrc
+ln -sfn $DOT_DIR/.gitconfig $HOME/.gitconfig
+ln -sfn $DOT_DIR/mimetypes/text-markdown.xml $HOME/.local/share/mime/packages/text-markdown.xml
 
 #
 # Extra steps - could do with a walk
 #
 nvim +PluginInstall +qall >> "$SETUP_LOG"
-
-python3 $HOME/.vim/bundle/YouCompleteMe/install.py --ts-completer --clang-completer --java-completer >> "$SETUP_LOG"
-
-ln -sfn $DOT_DIR/mimetypes/text-markdown.xml $HOME/.local/share/mime/packages/text-markdown.xml
 update-mime-database ~/.local/share/mime
+python3 $HOME/.vim/bundle/YouCompleteMe/install.py --ts-completer --clang-completer --java-completer >> "$SETUP_LOG"
+sudo usermod -a -G video "$USER" # Enables lighting control
 
 # Finally ask to change shell to zsh
 chsh -s /usr/bin/zsh
