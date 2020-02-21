a2453717f9109ad95400b2aaeeb701dcf2263c42
EddieB <eddiebergmanhs@gmail.com>
2020-02-21 15:34:50 +0000

    Added template 'link' functionality

--- .zsh/template.zsh
+++ .zsh/template.zsh
@@ -1,7 +1,8 @@
-# template
+#!/bin/bash
 # Creates templates by copying from a preconfigured template folder
 # or by calling a template handler that performs extra steps
 # See template_help for more
+# TODO: Implement add and edit
 source "$drzsh/template.help.zsh"
 
 template_root="$drtemplate"
@@ -14,30 +15,49 @@ template() {
     arg $1 "list" && shift && template_list "$@" && return
     arg $1 "read" && shift && template_read "$@" && return
     arg $1 "note" && shift && template_note "$@" && return
+    arg $1 "link" && shift && template_link "$@" && return
+    arg $1 "add" && shift && template_add "$@" && return
     arg $1 "edit" && $EDITOR $template_root && return
     arg $1 "--help" "-h" || lt $# 2 && template_help && return
+    ! two $# && template_help && return
 
     local name=$1
     local outpath=$2
-    if two $#; then
-        if ! writable "$(dirname $outpath)"; then
-            printf "Can't write to $(dirname $outpath), does direcotry exist?\n"; return; fi
+    if ! writable "$(dirname $outpath)"; then
+        printf "Can't write to $(dirname $outpath), does direcotry exist?\n"; return; fi
 
-        filepath="$(find $template_root -name $name)"
-
-        case "$(filecount $name $template_root)" in
-            "1") cp $filepath $outpath; echo "copied";
-                return;;
-            "0") echo "$name was not found in templates"; 
-                return;;
-            *) echo "$name was matched to too many files\n$filepath"; 
-                return;;
-        esac
+    local fpath="$(find $template_root -name $name)"
+    if emptyvar "$fpath"; then
+        print "$name was not found"
+        return
+    elif uniquefile $fpath; then
+        cp $fpath $outpath && echo "copied"
+        return
+    else
+       printf "$name was matched to too many files\n $fname \n"
+       return
     fi
 }
 
 template_link() {
+    ! two $# && template_link_cdoc && return
 
+    local name=$1
+    local outpath=$2
+    if ! writable "$(dirname $outpath)"; then
+        printf "Can't write to $(dirname $outpath), does direcotry exist?\n"; return; fi
+
+    local fpath="$(find $template_root -name $name)"
+    if emptyvar "$fpath"; then
+        print "$name was not found"
+        return
+    elif uniquefile $fpath; then
+        ln -sfn $fpath $outpath && echo "linked"
+        return
+    else
+       printf "$name was matched to too many files\n $fname \n"
+       return
+    fi
 }
 
 template_read() {
@@ -111,6 +131,6 @@ template_note() {
 }
 
 template_add() {
-    echo "hello from "
+    echo "hello from add"
     return
 }
