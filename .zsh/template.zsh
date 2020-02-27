94fcd094223f140fb932a6467baba0dfed09f546
EddieB <eddiebergmanhs@gmail.com>
2020-02-27 13:33:06 +0000

    Template help into single file, can link to bin

--- .zsh/template.zsh
+++ .zsh/template.zsh
@@ -3,13 +3,50 @@
 # or by calling a template handler that performs extra steps
 # See template_help for more
 # TODO: Implement add and edit
-source "$drzsh/template.help.zsh"
 
 template_root="$drtemplate"
 template_dirs=($template_root/*/)
 
 template_note_preamble="$drtemplate/note/note_preamble.tex"
 template_note_template="$drtemplate/note/note_template.tex"
+template_help() {
+    printf "
+=============
+~ ${BBlue}template${NC}
+=============
+
+~ ${BGreen}Commands${NC}
+
+    $(template_cdoc)
+    $(template_list_cdoc)
+    $(template_read_cdoc)
+    $(template_edit_cdoc)
+    $(template_add_cdoc)
+    $(template_note_cdoc)
+    $(template_link_cdoc)
+
+~ ${BGreen}File management${NC}
+
+    For more removing or performing more complex operations, please do so manually
+    at ${UYellow}\$template_root${NC}.
+
+~ ${BYellow}Settings${NC}
+
+    ${Cyan}main-settings${NC}
+    ${UYellow}template_root${NC}
+    =$template_root
+
+    ${UYellow}EDITOR${NC}
+    =$EDITOR
+
+    ${Cyan}note-settings${NC}
+    ${UYellow}template_note_preamble${NC}
+    =$template_note_preamble
+
+    ${UYellow}template_note_template${NC}
+    =$template_note_template"
+    return
+}
 
 template() {
     arg $1 "list" && shift && template_list "$@" && return
@@ -38,6 +75,13 @@ template() {
        return
     fi
 }
+template_cdoc() {
+    printf "
+    ${Green}$ template ${Yellow}<name>${NC} ${Yellow}<path>${NC}${NC}
+    ${Green}--------------------------------------------------${NC}
+    Copies template ${Yellow}<name>${NC} to ${Yellow}<path>${NC}."
+    return
+}
 
 template_link() {
     ! two $# && template_link_cdoc && return
@@ -59,6 +103,13 @@ template_link() {
        return
     fi
 }
+template_link_cdoc() {
+    printf "
+    ${Green}$ template link ${Yellow}<name>${NC} ${Yellow}<path>${NC}${NC}
+    ${Green}--------------------------------------------------${NC}
+    Links the template ${Yellow}<name>${NC} to ${Yellow}<path>${NC}"
+    return
+}
 
 template_read() {
     if helparg $1; then
@@ -73,6 +124,13 @@ template_read() {
         *) echo "$name was matched to too many files\n$filepath";
             return;;
     esac
+}
+template_read_cdoc() {
+    printf "
+    ${Green}$ template read ${Yellow}<name>${NC}${NC}
+    ${Green}--------------------------------------------------${NC}
+    Read out the template with ${Yellow}<name>${NC}".
+    return
 }
 
 template_list() {
@@ -108,6 +166,14 @@ template_list() {
         return
     fi
 }
+template_list_cdoc() {
+    printf "
+    ${Green}$ template list [ ${Yellow}<folder>${NC}${Green} | folders ]${NC}
+    ${Green}--------------------------------------------------${NC}
+    List all templates, optionally specifying which ${Yellow}<folder>${NC}
+    Specifiy ${Green}folders${NC} instead to list all folders in ${UYellow}\$template${NC}."
+    return
+}
 
 template_note() {
     if ! exists $template_note_preamble; then
@@ -130,8 +196,35 @@ template_note() {
     printf "Created note $1"
     return
 }
+template_note_cdoc() {
+    printf "
+    ${Green}$ template note ${Yellow}<path>${NC}${NC}
+    ${Green}--------------------------------------------------${NC}
+    Creates folders, link tex preamble and copies tex template to ${Yellow}<path>${NC}."
+    return
+}
 
 template_add() {
     echo "hello from add"
+    return
+}
+template_add_cdoc() {
+    printf "
+    ${Green}$ template add [${Yellow}<folder>${NC}${Green}] ${Yellow}<file>${NC}
+    ${Green}--------------------------------------------------${NC}
+    Creates a copy of ${Yellow}<file>${NC} and stores it under ${UYellow}\$template_root${NC}.
+    Optionally, pass a ${Yellow}<folder>${NC} to store it in, creating one if it does not exists."
+    return
+}
+
+template_edit() {
+    echo "hello from add"
+    return
+}
+template_edit_cdoc() {
+    printf "
+    ${Green}$ template edit [${Yellow}<name>${NC}${Green}]${NC}
+    ${Green}--------------------------------------------------${NC}
+    Opens ${UYellow}\$EDITOR${NC} in ${UYellow}\$template_root${NC}."
     return
 }
