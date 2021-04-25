234107687d008e1846325a72c7012bfc5d3376db
EddieB <eddiebergmanhs@gmail.com>
2021-04-25 02:47:44 +0200

    Added proper italics for FiraCode

--- fonts.install.sh
+++ fonts.install.sh
@@ -15,3 +15,20 @@ fi
 ( fc-list | grep 'dejavu' ) || ( sudo pacman -Su  ttf-dejavu --needed --noconfirm >> "$SETUP_LOG" )
 ( pacman -Qi adobe-source-sans-pro-fonts ) || ( sudo pacman -Su adobe-source-sans-pro-fonts --needed --noconfirm >> "$SETUP_LOG" )
 ( pacman -Qi gnu-free-fonts ) || ( sudo pacman -Su adobe-source-sans-pro-fonts --needed --noconfirm >> "$SETUP_LOG" )
+
+# Italics version of Fira Code, has to be built from source
+if [[ !$( fc-list | grep 'Fira Mono') ]] then;
+    # Clone lib
+    git clone https://github.com/Avi-D-coder/fira-mono-italic $HOME/fira-mono-italic
+
+    # There's probably a nice way to do this
+    [[ -d "${HOME}/.local/share/fonts" ]] || mkdir "${HOME}/.local/fonts"
+    [[ -d "${HOME}/.local/share/fonts/otf" ]] || mkdir "${HOME}/.local/share/fonts/otf"
+    mkdir "${HOME}/.local/share/fonts/otf/fira-mono"
+
+    cp $HOME/fira-mono-italic/distr/otf/* $HOME/.local/share/fonts/otf/fira-mono
+    rm -rf $HOME/fira-mono-italic
+fi
+
+# Update the font cache
+fc-cache
