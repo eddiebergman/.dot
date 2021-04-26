36a567fc94b24e6b2c9c0b258a38a2cf569b1a5b
EddieB <eddiebergmanhs@gmail.com>
2021-04-26 17:01:37 +0200

    Small cleanup, rcfiles, misc

--- i3extra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-i3status --config ~/.config/i3status/config | while :
-do
-    read line
-    LG=$(setxkbmap -query | awk '/layout/{print $2}')
-    echo "Keys: $LG | $line" || exit 1
-done
