553914e7cb27a0c80b2893e867ac89cd4ea0f9d5
eddiebergman <eddiebergmanhs@gmail.com>
2021-05-02 16:11:47 +0200

    Added lua util

---
+++ .config/nvim/lua/util.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+function M.system_name()
+    if vim.fn.has("mac") == 1 then
+        return "macOS"
+    elseif vim.fn.has("unix") == 1 then
+        return "Linux"
+    elseif vim.fn.has("win32") then
+        return "Windows"
+    else
+        print("system_name(): Unknown System")
+        return -1
+    end
+end
+
+function M.get(a, b)
+    if not a == nil then return a else return b end
+end
+
+function M.os_exec(cmd)
+    local handle = io.popen(cmd .. "; echo $?")
+    local os_result = handle:read("*all")
+    local cmd_result = os_result:gsub("\n[^\n]*(\n?)$", "%1")
+    local exit_status = os_result:match("(%d+)\n?$")
+    handle:close()
+    return cmd_result, exit_status
+end
+
+return M
