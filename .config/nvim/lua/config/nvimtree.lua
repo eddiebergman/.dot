6a2c885a711bf08acf23cf4967664b2ec482919c
eddiebergman <eddiebergmanhs@gmail.com>
2021-05-06 16:38:23 +0200

    Config for nvimtree

---
+++ .config/nvim/lua/config/nvimtree.lua
@@ -0,0 +1,39 @@
+-- https://github.com/kyazdani42/nvim-tree.lua
+local nvimtree = require('nvim-tree.config')
+local tree_cv = nvimtree.nvim_tree_callback
+
+[[
+--vim.g.nvim_tree_bindings = {
+      ["<CR>"] = ":YourVimFunction()<cr>",
+      ["u"] = ":lua require'some_module'.some_function()<cr>",
+
+      -- default mappings
+      ["<CR>"]           = tree_cb("edit"),
+      ["o"]              = tree_cb("edit"),
+      ["<2-LeftMouse>"]  = tree_cb("edit"),
+      ["<2-RightMouse>"] = tree_cb("cd"),
+      ["<C-]>"]          = tree_cb("cd"),
+      ["<C-v>"]          = tree_cb("vsplit"),
+      ["<C-x>"]          = tree_cb("split"),
+      ["<C-t>"]          = tree_cb("tabnew"),
+      ["<"]              = tree_cb("prev_sibling"),
+      [">"]              = tree_cb("next_sibling"),
+      ["<BS>"]           = tree_cb("close_node"),
+      ["<S-CR>"]         = tree_cb("close_node"),
+      ["<Tab>"]          = tree_cb("preview"),
+      ["I"]              = tree_cb("toggle_ignored"),
+      ["H"]              = tree_cb("toggle_dotfiles"),
+      ["R"]              = tree_cb("refresh"),
+      ["a"]              = tree_cb("create"),
+      ["d"]              = tree_cb("remove"),
+      ["r"]              = tree_cb("rename"),
+      ["<C-r>"]          = tree_cb("full_rename"),
+      ["x"]              = tree_cb("cut"),
+      ["c"]              = tree_cb("copy"),
+      ["p"]              = tree_cb("paste"),
+      ["[c"]             = tree_cb("prev_git_item"),
+      ["]c"]             = tree_cb("next_git_item"),
+      ["-"]              = tree_cb("dir_up"),
+      ["hq"]              = tree_cb("close"),
+}
+]]
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,2 +1,10 @@
-require('config/chadtree')
-print('loaded plugins')
+local plugin_configs = {
+    "compe", "nvimtree"
+}
+
+for _, plugin in ipairs(plugin_configs) do
+    require("config/"..plugin)
+end
+
+
+
