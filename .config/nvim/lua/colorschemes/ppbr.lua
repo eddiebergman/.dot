3d4eee22f90a144d34d2b9c5b022e908f296e155
eddiebergman <eddiebergmanhs@gmail.com>
2021-05-22 20:25:10 +0200

    Updated Colorscheme

--- .config/nvim/lua/colorschemes/ppbr.lua
+++ .config/nvim/lua/colorschemes/ppbr.lua
@@ -2,6 +2,10 @@
 -- Colours
 -- =======
 local DullYellow = "#a5894A"
+local DullerYellow = "#715e33"
+local DefaultBackground = '#222532'
+local NoteBackground = '#24293A'
+local HighlightBackground = "#2d3243"
 local Grey = "#9d8875"
 local DarkerGrey = "#676765"
 local Green = "#2a7916"
@@ -14,56 +18,78 @@ local Orange = "#F79000"
 local Red = "#ca7375"
 local White = "#a29f92"
 
+-- ===========
+-- Basic setting
+-- ===========
+local FunctionDef = DarkerCyan
+local ClassDef = Red
+local Type = Red
+local MethodCall = BrighterViolet
+local FunctionCall = Violet
+local Variable = White
+local Structure = SkyBlue
+local Keyword = SkyBlue
+local Operator = DarkerGrey
+local Constant = DullYellow
+local String = Grey
+local Comment = DarkerGrey
+
+local Error = Red
+local Warning = Orange
+local Hint = Grey
+
 local highlights = {
     -- Base Syntax Types
-    Structure = { guifg = DullYellow, gui="bold" },
-    Type = { guifg = DullYellow },
+    Structure = { guifg = Structure, gui="bold" },
+    Type = { guifg =  Type },
     Todo = { guifg = SkyBlue, gui="bold" },
-    Identifier = { guifg = Red },
 
     -- Lsp
-    LspDiagnosticsDefaultError = { guifg = Red },
-    LspDiagnosticsUnderlineError = { gui="undercurl", guisp = Red },
+    LspDiagnosticsDefaultError = { guifg = Error },
+    LspDiagnosticsUnderlineError = { gui="undercurl", guisp = Error },
 
-    LspDiagnosticsDefaultWarning = { guifg = Orange },
-    LspDiagnosticsUnderlineWarning= { gui="undercurl", guisp = Orange },
+    LspDiagnosticsDefaultWarning = { guifg = Warning },
+    LspDiagnosticsUnderlineWarning= { gui="undercurl", guisp =  Warning },
 
-    LspDiagnosticsDefaultInformation = { guifg = Grey },
-    LspDiagnosticsUnderlineInformation= { gui="undercurl", guisp = Grey },
+    LspDiagnosticsDefaultInformation = { guifg = Hint },
+    LspDiagnosticsUnderlineInformation= { gui="undercurl", guisp = Hint },
 
-    LspDiagnosticsDefaultHint = { guifg = Grey },
-    LspDiagnosticsUnderlineHint= { gui="undercurl", guisp = Grey },
+    LspDiagnosticsDefaultHint = { guifg = Hint },
+    LspDiagnosticsUnderlineHint= { gui="undercurl", guisp = Hint },
 
     -- Editor
     CursorLineNr = { guifg = Grey, gui="bold" },
     LineNr = { guifg = Grey },
+    ColorColumn = { guibg = NoteBackground },
+    Folded = { guifg = DullerYellow, guibg = "NONE", gui = "NONE"},
 
     -- Tree Sitter (General)
-    TSVariableBuiltin = { guifg = White, gui="bold" },
-    TSConstant = { guifg = Red, gui="nocombine,NONE" },
-    TSType = { guifg = Violet, gui="NONE" },
-    TSKeyword = { guifg = SkyBlue },
-    TSVariable = { guifg = White },
-    TSProperty = { guifg = White },
-    TSParameter = { guifg = White },
-    TSConditional = { guifg = SkyBlue },
-    TSRepeat = { guifg = SkyBlue },
-    TSString = { guifg = Grey },
-    TSKeywordOperator = { guifg = SkyBlue },
-    TSOperator = { guifg = DarkerGrey },
-    TSMethod = { guifg = DarkerCyan },
-    TSFuncBuiltin = { guifg = Violet, gui = "nocombine,NONE"},
-    TSFunction = { guifg = Red, gui="NONE" },
-    TSPunctBracket = { guifg = DarkerGrey },
-    TSPunctDelimiter = { guifg = DarkerGrey },
-    TSField = { guifg = White },
-    TSConstructor = { guifg = Red, gui="nocombine,NONE" },
-    TSConstBuiltin = { guifg = BrighterViolet, gui="NONE" },
-    TSPunctSpecial = { guifg = BrighterViolet },
-    TSNumber = { guifg = DullYellow },
-    TSComment = { guifg = DarkerGrey, gui="italic" },
-    TSInclude = { guifg = BrighterViolet },
-    TSKeywordFunction = { guifg = SkyBlue }
+    TSVariableBuiltin = { guifg = Variable, gui="bold" },
+    TSConstant = { guifg = Type, gui="nocombine,NONE" },
+    TSType = { guifg = Type, gui="NONE" },
+    TSKeyword = { guifg = Keyword },
+    TSVariable = { guifg = Variable },
+    TSProperty = { guifg = Variable },
+    TSParameter = { guifg = Variable },
+    TSConditional = { guifg = Keyword },
+    TSRepeat = { guifg = Keyword },
+    TSString = { guifg = String },
+    TSKeywordOperator = { guifg = Keyword },
+    TSOperator = { guifg = Operator },
+    TSMethod = { guifg =  MethodCall },
+    TSFuncBuiltin = { guifg = FunctionCall, gui = "nocombine,NONE"},
+    TSFunction = { guifg = DarkerCyan, gui="NONE" },
+    TSPunctBracket = { guifg = Operator },
+    TSPunctDelimiter = { guifg = Operator },
+    TSField = { guifg = Variable },
+    TSConstructor = { guifg = FunctionDef, gui="nocombine,NONE" },
+    TSConstBuiltin = { guifg = Constant, gui="NONE" },
+    TSPunctSpecial = { guifg = Operator },
+    TSNumber = { guifg = Constant },
+    TSComment = { guifg = Comment, gui="italic" },
+    TSInclude = { guifg = Violet },
+    TSKeywordFunction = { guifg = Keyword },
+    TSBoolean = { guifg = Constant }
 
 }
 
