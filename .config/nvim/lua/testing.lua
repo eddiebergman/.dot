766eec7d4ed7d0654390eeee0fb7fba3adb44e87
eddiebergman <eddiebergmanhs@gmail.com>
2022-03-29 22:33:34 +0200

    Reconfigure testing hotkeys

--- .config/nvim/lua/testing.lua
+++ .config/nvim/lua/testing.lua
@@ -4,9 +4,9 @@ local util = require('util')
 
 function self.setup()
     util.setkeys('n', {
-        {'t', ':UltestNearest<cr>:UltestSummaryOpen<cr>'},
+        {'T', ':UltestNearest<cr>:UltestSummaryOpen<cr>'},
+        {'<leader>t', ':UltestSummary<cr>'},
         {'<leader>to', ':UltestOutput<cr>'},
-        {'<C-t>', ':UltestSummary<cr>'},
     })
 end
 
