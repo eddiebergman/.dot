8b4c4f9cf10974d1bb318dd7ff6402289834e4bd
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-21 15:24:12 +0200

    Just add more options, mostly for debugging

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -11,7 +11,11 @@ function self.set_indent_blankline()
 
     -- Character to use │
     vim.g.indent_blankline_char = ''
+    vim.g.indent_blankline_char_blankline = ' '
+    vim.g.indent_blankline_space_char_blankline = ' '
     vim.g.indent_blankline_context_char = '│'
+    vim.g.indent_blankline_context_char_blankline = '│'
+
 
     -- highlights current indent
     vim.g.indent_blankline_show_current_context = true
