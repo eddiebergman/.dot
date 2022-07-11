3e0c3156e2c2c74e350516ab4fc6cf0bd4b87cd5
eddiebergman <eddiebergmanhs@gmail.com>
2022-07-11 23:38:19 +0200

    Fix nvimtree with update

--- .config/nvim/lua/tree.lua
+++ .config/nvim/lua/tree.lua
@@ -1,12 +1,10 @@
+-- If configuring this again, check out their dispatcher and follow what they
+-- do to figure it out
 local self = {}
 local util = require("util")
 local lib = require("nvim-tree.lib")
 local view = require("nvim-tree.view")
 
-
-local function collapse_all()
-    require("nvim-tree.actions.collapse-all").fn()
-end
 
 local function edit_or_open()
     -- open as vsplit on current node
@@ -15,14 +13,14 @@ local function edit_or_open()
 
     -- Just copy what's done normally with vsplit
     if node.link_to and not node.nodes then
-        require('nvim-tree.actions.open-file').fn(action, node.link_to)
+        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
         view.close() -- Close the tree if file was opened
 
     elseif node.nodes ~= nil then
         lib.expand_or_collapse(node)
 
     else
-        require('nvim-tree.actions.open-file').fn(action, node.absolute_path)
+        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
         view.close() -- Close the tree if file was opened
     end
 
@@ -35,13 +33,13 @@ local function vsplit_preview()
 
     -- Just copy what's done normally with vsplit
     if node.link_to and not node.nodes then
-        require('nvim-tree.actions.open-file').fn(action, node.link_to)
+        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
 
     elseif node.nodes ~= nil then
         lib.expand_or_collapse(node)
 
     else
-        require('nvim-tree.actions.open-file').fn(action, node.absolute_path)
+        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
 
     end
 
@@ -114,7 +112,7 @@ local config = {
                 { key = "l", action = "edit", action_cb = edit_or_open },
                 { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
                 { key = "h", action = "close_node" },
-                { key = "H", action = "collapse_all", action_cb = collapse_all },
+                { key = "H", action = "collapse_all"},
                 { key = "t", action = "toggle_git_ignored" }
             }
         },
