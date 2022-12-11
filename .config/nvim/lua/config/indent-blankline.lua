f31ac93185fb4d2daa5b6c419ca317c2996212b7
eddiebergman <eddiebergmanhs@gmail.com>
2022-12-11 10:48:46 +0100

    style: Only current context line shown

--- .config/nvim/lua/config/indent-blankline.lua
+++ .config/nvim/lua/config/indent-blankline.lua
@@ -3,9 +3,11 @@ local M = {}
 function M.setup()
     require("indent_blankline").setup({
         space_char_blankline = " ",
-        indent_blankline_char = "⎸",
+        char = " ",
         show_current_context = true,
         show_current_context_start = false,
+        show_first_indent_level = false,
+        context_char = "│",
     })
 end
 
